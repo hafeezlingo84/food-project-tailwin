8eb19395e6a49566fbb1ccce4baf790f88e4cc67
Muhammad Hafeez <75580479+hafeezlingo84@users.noreply.github.com>
2022-09-17 00:01:19 +0500

    react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './images/food-logo.png';
+import logo from '../images/food-logo.png';
 import { MdShoppingBasket } from "react-icons/md";
+import avatar from '../images/avatar.png';
 
  const Header = () => {
   return (
@@ -15,20 +16,21 @@ import { MdShoppingBasket } from "react-icons/md";
                     <p className='text-headingColor text-xl font-bold'>Food Resturent</p>
                 </div>
 
-                <ul className='flex items-center gap-8'>
+            <div className='flex items-center gap-8'>
+            <ul className='flex items-center gap-8'>
                   <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
                   <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
                   <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
                   <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
                 </ul>
-
                 <div className='relative flex items-center justify-center'>
-                  <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
+                  <MdShoppingBasket className='text-textColor text-2xl   cursor-pointer' />
                   <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
                     <p className='text-xs text-textWhite font-semibold'>2</p>
                   </div>
                 </div>
-
+                <img src={avatar} className="w-10 min-w-[40px] min-h-[40px]" alt="userProfile" />
+            </div>
           </div>
 
         {/* {mobile} */}
