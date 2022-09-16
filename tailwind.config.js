04446482a22e0ff75187b836e563e9a3b654a17c
Muhammad Hafeez <75580479+hafeezlingo84@users.noreply.github.com>
2022-09-16 21:41:11 +0500

    food-project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      headingColor: "#2e2e2e",
+      textColor: "#515151",
+      cartNumBg: "#e80013",
+      primary: "#f5f3",
+      textWhite:"white"
+    },
+  },
+  plugins: [],
+}
